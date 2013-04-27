81c97bd7c480954c46e62172a5c6843a09989531
Jérémy Schoffen <jschoffen.dev@gmail.com>
2013-04-27 02:01:49 +0200

    New ideas.

--- src/convert/core.clj
+++ src/convert/core.clj
@@ -1,2 +1,96 @@
-(ns convert.core)
+(ns convert.core
+  (:refer-clojure :exclude (== !=))
+  (:use clojure.core.logic))
 
+(defrel conversions from to f)
+
+
+(defn add-conversion 
+  ([t1 t2 t1->2]
+   (fact conversions t1 t2 t1->2))
+  ([t1 t2 t1->2 t2->1]
+   (do
+     (add-conversion t1 t2 t1->2)
+     (add-conversion t2 t1 t2->1))))
+
+
+
+
+(comment
+  )
+
+(add-conversion :mm :cm :div-10 :*10)
+(add-conversion :cm :dm :div-10)
+
+(search-inverse? :cm :dm)
+
+(add-conversion :a :b :a-to-b :b-to-a)
+(add-conversion :b :c :b-to-c :c-to-b)
+
+
+(defn not-membero [x l]
+  (conde 
+    [(== l '())]
+    [(fresh [head tail]
+       (conso head tail l)
+       (!= x head)
+       (not-membero x tail))]))
+
+;; inverse the way we discard froms
+;; instead of 
+;; we could  also search for the inverse of the inverse 
+;; before constructing a path
+(defn conversionso 
+  ([from to fns]
+   (conversionso '() from to fns))
+  
+  ([excluded from to fns]
+   (not-membero from excluded)
+   (fresh [?fn] 
+     (conde
+       [(conversions from to ?fn)
+        (== fns [?fn])]
+       
+       [(fresh [?excluded ?from ?fns]
+          (conversions from ?from ?fn)
+          (conso from excluded ?excluded)
+          (conversionso ?excluded ?from to ?fns)  
+          (conso ?fn ?fns fns))]))))
+
+
+
+
+
+(defn conv [from to]
+  (first
+   (run 1 [c]
+     (conversions from to c))))
+
+
+(defn search-inverse [t1 t2]
+  (first
+   (run 1 [c]
+     (conde
+       [(conversions t2 t1 c)]
+       [(fresh [?c ?tx ?ty]
+          (conversions t1 t2   ?c)
+          (conversions ?tx ?ty ?c)
+          (conversions ?ty ?tx  c))]))))
+
+
+(defn search-conversion [from to]
+  (run* [q]
+    (conversionso from to q)))
+
+
+
+
+(-> (get-conversion :cm :km) first reverse)
+(get-conversion :b :a)
+(get-conversion :b :c)
+
+(comment
+(defn convert [val to-type]
+  (let [c (first (conversion (type val) to-type))]
+    (c val)))
+)
--- test/convert/core_test.clj
+++ test/convert/core_test.clj
@@ -1,3 +1,37 @@
 (ns convert.core-test
   (:use convert.core
         midje.sweet))
+
+(add-conversion ::a ::b ::a-b)
+(add-conversion ::c ::d ::c-d ::d-c)
+
+
+
+
+(facts "The simple ones..."
+  (fact "We can specify conversions and find them"
+    (conv ::a ::b) => ::a-b
+    )
+  
+  (fact "we can look for the inverse of a conversions")
+  
+  (fact "we can remove conversions"))
+
+
+(facts "More funky ones"
+  (fact "We can look for the inverse of a conversion"
+    (comment "use conv"))
+  
+  (fact "If it doesnt exists we can try to look for it"
+    (comment "use search-inverse"))
+  
+  (fact (str "If a conversion doesn't exists but its inverse "
+             "does maybe its inverse inverse is used and already exists")
+    (comment "use inverse inverse")))
+
+(facts "Going crazy"
+  (fact (str "If a conversion doesnt exists we can try to find an equivalent compositions "
+             "of conversions which is transiently equivalent"))
+  
+  (facts (str "Since we can compose conversions to find new ones "
+              "we should be able to find inverses that we couldn't before")))
